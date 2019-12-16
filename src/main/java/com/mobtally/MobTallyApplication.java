365d6d19ad4669d4f94d0def4cd575218f3bab97
Sathish <sathish.2009btech@gmail.com>
2019-12-16 08:46:27 +0530

    main application

---
+++ src/main/java/com/mobtally/MobTallyApplication.java
@@ -0,0 +1,11 @@
+package com.mobtally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobTallyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MobTallyApplication.class, args);
+    }
+}
