2f0e846b8456238d92089457ab084b1b1efc53db
Sathish <sathish.2009btech@gmail.com>
2020-01-21 07:13:24 +0530

    postgres connection success

--- src/main/java/com/mobtally/domain/JpaCompany.java
+++ src/main/java/com/mobtally/domain/JpaCompany.java
@@ -16,6 +16,22 @@ public class JpaCompany {
     @Column(name = "company_id")
     private Long id;
 
-    @Column(name = "xml_data")
-    private String xmlData;
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
